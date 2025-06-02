c4ecdd7e7f14d6b63d1adad497bb105f92292c82
Cody Gardi <35705693+codygardi@users.noreply.github.com>
2025-06-02 14:10:35 -0700

    Update employee_generator.py

--- modules/employee_generator.py
+++ modules/employee_generator.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import random
 import json
+import string
 from datetime import datetime, timedelta
 from modules.db_manager import save_employees
 from modules.scheduler_engine import RULES
+from faker import Faker
 
 LOCATIONS = RULES['active_locations']
 SHIFT_TYPES = RULES['shift_types']
@@ -17,25 +19,38 @@ SKILL_LEVELS = ["Tech1", "Tech2", "Tech3"]
 
 def generate_employees(n=30, seed=42):
     random.seed(seed)
+    fake = Faker()
+    Faker.seed(seed)
+
+    def generate_id():
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
     employees = []
     today = datetime.today()
+    used_ids = set()
 
-    for i in range(n):
-        emp_id = f"E{i:03}"
+    for _ in range(n):
+        emp_id = generate_id()
+        while emp_id in used_ids:
+            emp_id = generate_id()
+        used_ids.add(emp_id)
+
         hire_date = today - timedelta(days=random.randint(30, 1000))
         work_pattern = random.choice(WORK_PATTERNS)
         preferred_locations = random.sample(LOCATIONS, k=random.choice([1, 2]))
         preferred_shifts = random.sample(SHIFT_TYPES, k=random.choice([1, len(SHIFT_TYPES)]))
         skill_level = random.choice(SKILL_LEVELS)
-
-        # Generate 1–3 unavailable days in the next 2 weeks
+        full_name = fake.name()
+        phone_number= fake.phone_number()
         unavailable_days = sorted(list({
-            (today + timedelta(days=random.randint(0, 13))).strftime('%Y-%m-%d')
+            (today + timedelta(days=random.randint(8, 24))).strftime('%Y-%m-%d')
             for _ in range(random.randint(1, 3))
         }))
 
         employees.append({
             "EmployeeID": emp_id,
+            "Name": full_name,
+            "PhoneNumber": phone_number,
             "DateHired": hire_date.strftime('%Y-%m-%d'),
             "WorkPattern": json.dumps(work_pattern),
             "PreferredLocations": json.dumps(preferred_locations),
